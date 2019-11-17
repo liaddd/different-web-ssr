1db803161446ae010932ddc05a323fecd107b4d0
ildevelop <raduilya@gmail.com>
2019-11-18 01:51:11 +0200

    [fix] blog item path

--- pages/blog/post.js
+++ pages/blog/post.js
@@ -5,23 +5,31 @@ import Footer from "../../components/Footer/Footer";
 import Head from "../head";
 import PageHeader from "../../components/PageHeader/PageHeader";
 import Butter from "buttercms";
+import ErrorPage from "../_error";
 
 const butter = Butter("8a5fd9fe559d9c892ade6dfef4614676deb5d08c");
 
 class PostDetails extends Component {
-  static async getInitialProps({ query }) {
-    const resp = await butter.post.retrieve(query.title);
-    return resp.data;
+  static async getInitialProps({ query, res }) {
+    try {
+      const resp = await butter.post.retrieve(query.title.split("/")[1]);
+      return resp.data;
+    } catch (e) {
+      console.error("ERROR! blog not found");
+      return { err: { statusCode: 404 } };
+    }
   }
   render() {
-    const post = this.props.data;
-
+    const { data, err } = this.props;
+    if (err) {
+      return <ErrorPage statusCode={err.statusCode} />;
+    }
     return (
       <Head title="DiffeRent" ogTitle="בלוג" canonical="blog">
         <Header />
-        <PageHeader PageHeaderTitle={`${post.title}`} />
+        <PageHeader PageHeaderTitle={`${data.title}`} />
         <div>
-          <div dangerouslySetInnerHTML={{ __html: post.body }} />
+          <div dangerouslySetInnerHTML={{ __html: data.body }} />
         </div>
         <Footer />
       </Head>
