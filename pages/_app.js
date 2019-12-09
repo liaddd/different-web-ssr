5fce464344d58be13ab9b9cf28647a472c80c8a6
Liad Horovitz <liaddd@gmail.com>
2019-12-10 00:26:49 +0200

    added hotjar analytics to web

--- pages/_app.js
+++ pages/_app.js
@@ -1,32 +1,32 @@
-import { Provider } from "react-redux";
-import App from "next/app";
-import withRedux from "../lib/with-redux";
-import TagManager from "react-gtm-module";
+import { Provider } from 'react-redux'
+import App from 'next/app'
+import withRedux from '../lib/with-redux'
+import { hotjar } from 'react-hotjar'
+import TagManager from 'react-gtm-module'
 const tagManagerArgs = {
-  gtmId: "GTM-K9R7NGD"
-};
+  gtmId: 'GTM-K9R7NGD'
+}
 class MyApp extends App {
   static async getInitialProps({ Component, ctx }) {
     return {
       pageProps: {
-        ...(Component.getInitialProps
-          ? await Component.getInitialProps(ctx)
-          : {})
+        ...(Component.getInitialProps ? await Component.getInitialProps(ctx) : {})
       }
-    };
+    }
   }
   componentDidMount() {
-    TagManager.initialize(tagManagerArgs);
+    TagManager.initialize(tagManagerArgs)
+    hotjar.initialize(1356487, 6)
   }
 
   render() {
-    const { Component, pageProps, reduxStore } = this.props;
+    const { Component, pageProps, reduxStore } = this.props
     return (
       <Provider store={reduxStore}>
         <Component {...pageProps} />
       </Provider>
-    );
+    )
   }
 }
 
-export default withRedux(MyApp);
+export default withRedux(MyApp)
